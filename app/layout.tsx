79524d9dbdf582f9462f1e9dd3563ae4f68b9ec9
donghwan <kdhe9797@naver.com>
2025-03-20 17:28:25 +0900

    10장 완료

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 import React from "react";
+import SideNav from "./ui/dashboard/sidenav";
+
+export const experimental_ppr = true;
 
 export default function RootLayout({
   children,
